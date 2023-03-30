56f82e4e108292c4eb8b5eef09bffca507cd4f36
Tienhg2001 <tienhg201@gmail.com>
2023-03-30 23:41:36 +0700

    useEffect (ex1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import UseEffect from "./useEffect/UseEffect(ex1)";
 import UseState from "./useStateHook/UseState";
 
 function App() {
   return (
     <div className="App">
       <UseState></UseState>
+      <UseEffect></UseEffect>
     </div>
   );
 }
